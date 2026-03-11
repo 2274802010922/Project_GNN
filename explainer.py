de55af3e820eb1a3190b7a30c3513715b7ef718c
2274802010922 <habang5233@gmail.com>
2026-03-11 18:28:29 +0700

    Refactor explainer function and update parameters
    Refactor the explainer function to use 'graph' instead of 'data' and update the algorithm's epochs from 200 to 100.

--- explainer.py
+++ explainer.py
@@ -1,25 +1,20 @@
-from torch_geometric.explain import Explainer,GNNExplainer
+from torch_geometric.explain import Explainer, GNNExplainer
 
-
-def run_explainer(model,data):
+def explain(model, graph):
 
     explainer = Explainer(
         model=model,
-        algorithm=GNNExplainer(epochs=200),
+        algorithm=GNNExplainer(epochs=100),
         explanation_type='model',
         node_mask_type='attributes',
         edge_mask_type='object',
         model_config=dict(
             mode='multiclass_classification',
-            task_level='node',
+            task_level='graph',
             return_type='raw'
         ),
     )
 
-    explanation = explainer(
-        x=data.x,
-        edge_index=data.edge_index,
-        index=0,
-    )
+    explanation = explainer(graph.x, graph.edge_index)
 
     return explanation
