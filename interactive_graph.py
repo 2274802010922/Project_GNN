15bf1e9fd0011c1a7c127f45c95f8b7aa19b8bfa
2274802010922 <habang5233@gmail.com>
2026-03-11 20:06:50 +0700

    Create interactive_graph.py

---
+++ interactive_graph.py
@@ -0,0 +1,42 @@
+from pyvis.network import Network
+import networkx as nx
+
+
+def visualize_interactive_graph(data, image_paths, edge_mask=None):
+
+    G = nx.Graph()
+
+    edge_index = data.edge_index.cpu().numpy()
+
+    for i in range(data.num_nodes):
+
+        img_path = image_paths[i]
+
+        G.add_node(
+            i,
+            title=f'<img src="{img_path}" width="120">',
+            label=str(i)
+        )
+
+    for i in range(edge_index.shape[1]):
+
+        src = int(edge_index[0][i])
+        dst = int(edge_index[1][i])
+
+        color = "gray"
+        width = 1
+
+        if edge_mask is not None:
+            importance = edge_mask[i].item()
+
+            if importance > 0.5:
+                color = "red"
+                width = 4
+
+        G.add_edge(src, dst, color=color, width=width)
+
+    net = Network(height="750px", width="100%", notebook=True)
+
+    net.from_nx(G)
+
+    net.show("graph.html")
