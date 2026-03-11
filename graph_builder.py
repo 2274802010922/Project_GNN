5f35987ff6cee8f3847ef965becd506b45ff5f1d
2274802010922 <habang5233@gmail.com>
2026-03-11 20:32:49 +0700

    Update graph_builder.py

--- graph_builder.py
+++ graph_builder.py
@@ -1,24 +1,32 @@
 import torch
 from torch_geometric.data import Data
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 def build_graph(features):
 
-    x = torch.stack(features)
+    features = np.array(features)
 
-    num_nodes = x.shape[0]
+    similarity = cosine_similarity(features)
 
-    edges = []
+    edge_index = []
 
-    for i in range(num_nodes - 1):
-        edges.append([i, i + 1])
-        edges.append([i + 1, i])
+    threshold = 0.9
 
-    edge_index = torch.tensor(edges).t().contiguous()
+    for i in range(len(features)):
+        for j in range(len(features)):
+
+            if i != j and similarity[i][j] > threshold:
+                edge_index.append([i, j])
+
+    edge_index = torch.tensor(edge_index).t().contiguous()
+
+    x = torch.tensor(features, dtype=torch.float)
 
     data = Data(x=x, edge_index=edge_index)
 
-    print("Graph nodes:", num_nodes)
-    print("Graph edges:", edge_index.shape[1])
+    print("Nodes:", data.num_nodes)
+    print("Edges:", data.num_edges)
 
     return data
