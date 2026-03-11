b93e29a029fc4c35b362c89bffb3df619487bdc8
2274802010922 <habang5233@gmail.com>
2026-03-11 18:39:02 +0700

    Refactor GNNModel to GCN with updated structure

--- model.py
+++ model.py
@@ -1,28 +1,22 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, global_mean_pool
-import torch.nn as nn
+from torch_geometric.nn import GCNConv
 
-class GNNModel(nn.Module):
 
-    def __init__(self, input_dim, hidden_dim, num_classes):
+class GCN(torch.nn.Module):
+
+    def __init__(self,input_dim,hidden_dim=128):
+
         super().__init__()
 
-        self.conv1 = GCNConv(input_dim, hidden_dim)
-        self.conv2 = GCNConv(hidden_dim, hidden_dim)
+        self.conv1 = GCNConv(input_dim,hidden_dim)
+        self.conv2 = GCNConv(hidden_dim,hidden_dim)
 
-        self.lin = nn.Linear(hidden_dim, num_classes)
+    def forward(self,x,edge_index):
 
-    def forward(self, x, edge_index, batch):
-
-        x = self.conv1(x, edge_index)
+        x = self.conv1(x,edge_index)
         x = F.relu(x)
 
-        x = self.conv2(x, edge_index)
-        x = F.relu(x)
-
-        x = global_mean_pool(x, batch)
-
-        x = self.lin(x)
+        x = self.conv2(x,edge_index)
 
         return x
