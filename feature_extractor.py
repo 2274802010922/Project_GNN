d8fee7c077f931d78b8a94642160c906a456afa4
2274802010922 <habang5233@gmail.com>
2026-03-11 20:26:47 +0700

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,40 +1,40 @@
-import torch
-import torchvision.models as models
-import torchvision.transforms as transforms
-from PIL import Image
 import os
-
-
-model = models.resnet18(weights="DEFAULT")
-model = torch.nn.Sequential(*list(model.children())[:-1])
-model.eval()
-
-
-transform = transforms.Compose([
-    transforms.Resize((224,224)),
-    transforms.ToTensor()
-])
-
+import torch
+from torchvision import models, transforms
+from PIL import Image
 
 def extract_features(dataset_path):
 
+    model = models.resnet18(pretrained=True)
+    model = torch.nn.Sequential(*list(model.children())[:-1])
+    model.eval()
+
+    transform = transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor()
+    ])
+
     features = []
+    image_paths = []   # ← THÊM DÒNG NÀY
 
     for root, dirs, files in os.walk(dataset_path):
 
-        for f in files:
+        for file in files:
 
-            if f.endswith(".jpg"):
+            if file.endswith(".png") or file.endswith(".jpg"):
 
-                img_path = os.path.join(root, f)
+                img_path = os.path.join(root, file)
 
                 img = Image.open(img_path).convert("RGB")
                 img = transform(img).unsqueeze(0)
 
                 with torch.no_grad():
-                    feat = model(img)
+                    feature = model(img)
 
-                features.append(feat.squeeze())
+                feature = feature.squeeze().numpy()
+
+                features.append(feature)
+                image_paths.append(img_path)   # ← THÊM DÒNG NÀY
 
     print("Total images:", len(features))
 
