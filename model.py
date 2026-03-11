a64dda6b2e8c13288a8022fd233ea582eab55c5f
2274802010922 <habang5233@gmail.com>
2026-03-11 18:27:31 +0700

    Refactor GCN to GNNModel with pooling and classification
    Refactor GCN class into GNNModel with additional linear layer and global mean pooling.

--- model.py
+++ model.py
@@ -1,20 +1,28 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, global_mean_pool
+import torch.nn as nn
 
-class GCN(torch.nn.Module):
+class GNNModel(nn.Module):
 
-    def __init__(self,input_dim,hidden_dim=128):
+    def __init__(self, input_dim, hidden_dim, num_classes):
         super().__init__()
 
-        self.conv1 = GCNConv(input_dim,hidden_dim)
-        self.conv2 = GCNConv(hidden_dim,hidden_dim)
+        self.conv1 = GCNConv(input_dim, hidden_dim)
+        self.conv2 = GCNConv(hidden_dim, hidden_dim)
 
-    def forward(self,x,edge_index):
+        self.lin = nn.Linear(hidden_dim, num_classes)
 
-        x = self.conv1(x,edge_index)
+    def forward(self, x, edge_index, batch):
+
+        x = self.conv1(x, edge_index)
         x = F.relu(x)
 
-        x = self.conv2(x,edge_index)
+        x = self.conv2(x, edge_index)
+        x = F.relu(x)
+
+        x = global_mean_pool(x, batch)
+
+        x = self.lin(x)
 
         return x
