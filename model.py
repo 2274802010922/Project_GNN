93b35217840718b84f124f2737f2888012b63137
2274802010922 <habang5233@gmail.com>
2026-03-11 18:05:44 +0700

    Rename GCN to GNN and add classification layer
    Refactor GCN class to GNN and add linear layer for classification.

--- model.py
+++ model.py
@@ -1,19 +1,27 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, global_mean_pool
 
-class GCN(torch.nn.Module):
+class GNN(torch.nn.Module):
 
-    def __init__(self, input_dim, hidden_dim=128):
-        super().__init__()
+    def __init__(self, input_dim, hidden_dim, num_classes):
+        super(GNN, self).__init__()
 
         self.conv1 = GCNConv(input_dim, hidden_dim)
         self.conv2 = GCNConv(hidden_dim, hidden_dim)
 
-    def forward(self, x, edge_index):
+        self.lin = torch.nn.Linear(hidden_dim, num_classes)
+
+    def forward(self, x, edge_index, batch):
 
         x = self.conv1(x, edge_index)
         x = F.relu(x)
+
         x = self.conv2(x, edge_index)
+        x = F.relu(x)
+
+        x = global_mean_pool(x, batch)
+
+        x = self.lin(x)
 
         return x
